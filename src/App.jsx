5392d24622386363cccbd17387bd7a2be2d2c32c
dawoodkaleem <dawood9743@gmail.com>
2024-09-20 21:14:36 +0500

    App js added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0);
-  console.log(process.env.REACT_APP_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <div>
