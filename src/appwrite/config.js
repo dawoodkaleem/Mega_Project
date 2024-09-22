c9be1796943cc78c4e128cedc1072f86df1fd599
dawoodkaleem <dawood9743@gmail.com>
2024-09-22 12:31:03 +0500

    config completed for now

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -99,6 +99,18 @@ export class Services {
       return false;
     }
   }
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile :: error ", error);
+      return false;
+    }
+  }
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
 }
 
 const service = new Services();
