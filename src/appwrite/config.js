4a57f42b26a1926d2191838ebf44bf555a631913
dawoodkaleem <dawood9743@gmail.com>
2024-09-22 12:18:31 +0500

    GetPost functionalit added

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -13,6 +13,78 @@ export class Services {
     this.dataases = new Databases(this.client);
     this.bucket = new Storage(this.client);
   }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.dataases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: createPost :: error ", error);
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.dataases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: updatePost :: error ", error);
+    }
+  }
+  async deletePost(slug) {
+    try {
+      await this.dataases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: deletePost :: error ", error);
+      return false;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.dataases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPost :: error ", error);
+      return false;
+    }
+  }
+  async getPost(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.dataases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPost :: error ", error);
+      return false;
+    }
+  }
 }
 
 const service = new Services();
