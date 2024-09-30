ac79e8a673b4b5c0fcefecea9c739a0fab35042c
dawoodkaleem <dawood9743@gmail.com>
2024-09-30 21:06:52 +0500

    updated App .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <div>
-        <h1>A blog app with appwrite</h1>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className=" min-h-screen  flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main> TODO: {/* <Outlet /> */}</main>
+        <Footer />
       </div>
-    </>
-  );
+    </div>
+  ) : null;
 }
 
 export default App;
