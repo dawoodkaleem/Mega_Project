2346d3f1de6af9289338e8b0c70217bf874e86f5
dawoodkaleem <dawood9743@gmail.com>
2024-10-01 13:56:43 +0500

    index Header Updated

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+export default { Header, Footer, Container, Logo, LogoutBtn };
