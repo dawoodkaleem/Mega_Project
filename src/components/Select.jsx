e0eed35e2ccc46877a6f1d954ea04165842722ca
dawoodkaleem <dawood9743@gmail.com>
2024-10-13 17:28:14 +0500

    Select props validation

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -16,28 +16,46 @@
 // export default  React.forwardRef(Select);
 
 import React from "react";
+import PropTypes from "prop-types";
 
-function Select({ options, label, className, id, ...props }, ref) {
-  return (
-    <div className="w-full">
-      {label && (
-        <label htmlFor={id} className="block mb-2">
-          {label}
-        </label>
-      )}
-      <select
-        {...props}
-        id={id}
-        ref={ref}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
-        {options?.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-}
+const Select = React.forwardRef(
+  ({ options, label, className, id, ...props }, ref) => {
+    return (
+      <div className="w-full">
+        {label && (
+          <label htmlFor={id} className="block mb-2">
+            {label}
+          </label>
+        )}
+        <select
+          {...props}
+          id={id}
+          ref={ref}
+          className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}>
+          {options?.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+);
+Select.displayName = "Select";
 
-export default React.forwardRef(Select);
+Select.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  label: PropTypes.string,
+  className: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  props: PropTypes.object,
+};
+
+Select.defaultProps = {
+  label: "",
+  className: "",
+  props: {},
+};
+
+export default Select;
