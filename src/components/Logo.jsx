c4ebcdae2400fae073761e6567883577d2eb4f40
dawoodkaleem <dawood9743@gmail.com>
2024-10-13 17:24:30 +0500

    Error resolved

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,13 @@
 import {} from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 
 function Logo({ width = "100px" }) {
   return <div>Logo</div>;
 }
+
+// Add prop validation
+Logo.propTypes = {
+  width: PropTypes.string, // Expecting a string for width
+};
 
 export default Logo;
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,11 @@
 import {} from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
 
 function PostCard({ $id, title, featuredImage }) {
   return (
-    <Link to={`/post/$($id)`}>
-      {" "}
+    <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4">
         <div className="w-full justify-center mb-4">
           <img
@@ -19,5 +19,12 @@ function PostCard({ $id, title, featuredImage }) {
     </Link>
   );
 }
+
+// Add prop validation
+PostCard.propTypes = {
+  $id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  featuredImage: PropTypes.string.isRequired,
+};
 
 export default PostCard;
