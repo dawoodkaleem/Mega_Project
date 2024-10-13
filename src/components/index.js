83f31955dae81edb2161828912b204909c060e67
dawoodkaleem <dawood9743@gmail.com>
2024-10-13 17:30:09 +0500

    Error resolved

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,62 @@
+// import Header from "./Header/Header";
+// import Footer from "./Footer/Footer";
+// import Logo from "./Logo";
+// import Container from "./container/Container";
+// import LogoutBtn from "./Header/LogoutBtn";
+// import RTE from "./RTE";
+// import Signup from "./Signup";
+// import Login from "./Login";
+// // import Post from "./Post";
+// import PostForm from "./post-form/PostForm";
+// import PostCard from "./PostCard";
+// import AuthLayout from "./AuthLayout";
+// import Input from "./Input";
+// import Select from "./Select";
+// import Button from "./Button";
+// export default {
+//   Header,
+//   Footer,
+//   Container,
+//   Logo,
+//   LogoutBtn,
+//   RTE,
+//   Signup,
+//   Login,
+//   Button,
+//   // Post,
+//   PostForm,
+//   PostCard,
+//   AuthLayout,
+//   Input,
+//   Select,
+// };
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Logo from "./Logo";
 import Container from "./container/Container";
+import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-export default { Header, Footer, Container, Logo, LogoutBtn };
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
+};
