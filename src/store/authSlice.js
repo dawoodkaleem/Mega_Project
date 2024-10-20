50ff3c87051ce768415292d2e45aa0ba9f9496fe
dawoodkaleem <dawood9743@gmail.com>
2024-10-20 18:05:02 +0500

    created new functions

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -37,7 +37,9 @@ const authSlice = createSlice({
     login: (state, action) => {
       state.status = true;
       state.userData = action.payload.userData;
+      console.log(action.payload.userData, "Hello");
     },
+
     logout: (state) => {
       state.status = false;
       state.userData = null;
