a4cc76004ee6853083b4005f0cf5c65ab2c826a6
dawoodkaleem <dawood9743@gmail.com>
2024-10-20 18:02:48 +0500

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import {} from "react";
 import { Container, Logo, LogoutBtn } from "../index";
 import { Link } from "react-router-dom";
-import { useSelector } from "./react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 function Header() {
-  const authStatus = useSelector((state) => state.auth.status);
+  const authStatus = useSelector((state) => state.auth?.status);
   const navigate = useNavigate();
   const navItems = [
     {
