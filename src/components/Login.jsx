4b4353eef2eb88c1e30ee9839871a978b5d5b7ad
dawoodkaleem <dawood9743@gmail.com>
2024-11-25 17:47:31 +0500

    Updated code login function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -55,7 +55,7 @@ function Login() {
                 required: true,
                 validate: {
                   matchPatern: (value) =>
-                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*/.test(value) ||
                     "Email address must be a valid address",
                 },
               })}
